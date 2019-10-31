18139c689424bc8528a05640a3eb9c588f57596d
wah <2643567501@qq.com>
2019-10-31 15:46:35 +0800

    下拉框数据字典操作方法接口

---
+++ src/com/bbs/service/SelectorDictService.java
@@ -0,0 +1,7 @@
+package com.bbs.service;
+
+import com.bbs.po.SelectorDict;
+
+public interface SelectorDictService {
+	public SelectorDict selectSelectorDictByDictTypeCode(String dict_type_code);
+}
